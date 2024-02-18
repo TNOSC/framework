a11f1be129560040db558b9afb40701a80da2bd2
ahedfi <ahmed.hedfi@gmail.com>
2024-02-18 19:36:49 +0100

    add module building blocs #21

---
+++ src/Abstractions/Tnosc.Components.Abstractions.Module/IModule.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace Tnosc.Components.Abstractions.Module;
+
+/// <summary>
+/// Represents a modular component in the application.
+/// </summary>
+public interface IModule
+{
+    /// <summary>
+    /// Gets the name of the module.
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// Gets the policies associated with the module.
+    /// </summary>
+    /// <remarks>
+    /// Implementers can provide custom policies. Defaults to null if not overridden.
+    /// </remarks>
+    IEnumerable<string> Policies => null;
+
+    /// <summary>
+    /// Registers services related to the module.
+    /// </summary>
+    /// <param name="services">The service collection to register services.</param>
+    /// <param name="configuration">The configuration related to the module.</param>
+    void Register(IServiceCollection services, IConfiguration configuration);
+
+    /// <summary>
+    /// Configures middleware components for the module.
+    /// </summary>
+    /// <param name="app">The application builder to configure middleware.</param>
+    void Use(IApplicationBuilder app);
+}
---
+++ src/Infrastructure/Tnosc.Components.Infrastructure.Module/ModuleLoader.cs
@@ -0,0 +1,75 @@
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Tnosc.Components.Abstractions.Module;
+
+namespace Tnosc.Components.Infrastructure.Module;
+
+/// <summary>
+/// Utility class for loading assemblies and extracting modules.
+/// </summary>
+public static class ModuleLoader
+{
+    /// <summary>
+    /// Loads assemblies based on the provided configuration and module part.
+    /// </summary>
+    /// <param name="configuration">The configuration containing module-related settings.</param>
+    /// <param name="modulePart">The part of the module's file name to identify relevant assemblies.</param>
+    /// <returns>A list of loaded assemblies.</returns>
+    public static IList<Assembly> LoadAssemblies(IConfiguration configuration, string modulePart)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
+
+        // Get locations of already loaded assemblies
+        var locations = assemblies.Where(x => !x.IsDynamic).Select(x => x.Location).ToArray();
+
+        // Get all DLL files in the application's base directory
+        var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+            .Where(x => !locations.Contains(x, StringComparer.InvariantCultureIgnoreCase))
+            .ToList();
+
+        var disabledModules = new List<string>();
+
+        // Iterate through DLL files to filter by modulePart and check for module enabling
+        foreach (var file in files)
+        {
+            if (!file.Contains(modulePart))
+            {
+                continue;
+            }
+
+            var moduleName = file.Split(modulePart)[1].Split(".")[0].ToLowerInvariant();
+            var enabled = configuration.GetValue<bool>($"{moduleName}:module:enabled");
+
+            if (!enabled)
+            {
+                disabledModules.Add(file);
+            }
+        }
+
+        // Remove disabled modules from the list of files
+        foreach (var disabledModule in disabledModules)
+        {
+            files.Remove(disabledModule);
+        }
+
+        // Load remaining DLL files as assemblies
+        files.ForEach(x => assemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(x))));
+
+        return assemblies;
+    }
+
+    /// <summary>
+    /// Loads modules from the provided assemblies.
+    /// </summary>
+    /// <param name="assemblies">The list of assemblies containing modules.</param>
+    /// <returns>A list of loaded modules implementing the IModule interface.</returns>
+    public static IList<IModule> LoadModules(IEnumerable<Assembly> assemblies)
+        => assemblies
+            .SelectMany(x => x.GetTypes())
+            .Where(x => typeof(IModule).IsAssignableFrom(x) && !x.IsInterface)
+            .OrderBy(x => x.Name)
+            .Select(Activator.CreateInstance)
+            .Cast<IModule>()
+            .ToList();
+}
+
